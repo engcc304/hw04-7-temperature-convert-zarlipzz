4735783c19d8aca1d9d013894de916b6ea17edb8
zarlipzz <guitar30255@gmail.com>
2023-08-16 13:23:20 +0700

    ffgf

--- Homework/HW04/hw04-7-Temperator-Convert.cpp
+++ Homework/HW04/hw04-7-Temperator-Convert.cpp
@@ -13,3 +13,14 @@
     Output:
         36 degree Celsius = 96.8 degree Fahrenheit
 */
+#include <stdio.h>
+
+int main() {
+    float celsius, fahrenheit;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+    fahrenheit = (celsius * 9 / 5) + 32;
+    printf("%.2f degree Celsius = %.2f degree Fahrenheit\n", celsius, fahrenheit);
+
+    return 0;
+}
